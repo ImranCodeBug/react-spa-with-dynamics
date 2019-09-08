f9c74a544438d9adf6e80ca691ec3a22c177e847
ImranDCA <imran.chowdhury@doctorcareanywhere.com>
2019-09-08 14:14:11 +0100

    Added config

---
+++ src/Configs/AdalConfig.js
@@ -0,0 +1,11 @@
+export const AdalConfig = {
+    clientId : "e5a0e6fc-582c-46e2-a6a4-21402597b687",
+    tenant : "9047ed58-9700-4da0-a89d-d4f77972fe59",
+    redirectUri: "http://localhost:3000",
+    endpoints : {            
+        api : "https://kaisar2.crm11.dynamics.com" 
+    },
+    cacheLocation : "localStorage"
+}
+
+export default AdalConfig;
